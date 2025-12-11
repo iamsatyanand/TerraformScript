d66a0eae587454aeca10d2e3c9b0783936e5e471
iamsatyanand <iamsatyanandmishra@gmail.com>
2025-12-11 20:12:12 +0530

    terraform modularity examples

---
+++ 04-APP/input-var.tf
@@ -0,0 +1,9 @@
+variable "ami" {
+  description = "amazon ec2 instance"
+  default     = "ami-0d176f79571d18a8f"
+}
+
+variable "instance_type" {
+  description = "ec2 instance"
+  default     = "t3.micro"
+}
---
+++ 04-APP/main.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "linux_vm" {
+  ami             = var.ami
+  instance_type   = var.instance_type
+  key_name        = "terraform-kp"
+  security_groups = ["default"]
+  tags = {
+    Name = "Testing-server"
+  }
+}
